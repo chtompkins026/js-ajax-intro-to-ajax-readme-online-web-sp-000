24bf0295975fc715c2f18326e1199f5ff88f81cf
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 17:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,18 +29,19 @@ function showCommits() {
   document.getElementById('commits').innerHTML = commitsList;
 }
  
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
-}
 
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
 
